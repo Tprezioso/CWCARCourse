ef819351cbca8b99b72667b395c7208ccfaa3a9f
Thomas <tommyprezioso@gmail.com>
2022-08-31 20:57:58 -0400

    Initial Commit

---
+++ ARKitCourse/ARKitCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitCourseApp.swift
+//  ARKitCourse
+//
+//  Created by Thomas Prezioso Jr on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct ARKitCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
